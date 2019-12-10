2bf277cb342c8f3064bf20d4f3ebf2f73d29e296
dreamflyer <gleb@onpositive.com>
2019-12-11 05:19:47 +0700

    minor fix

--- musket_server/main.py
+++ musket_server/main.py
@@ -3,14 +3,14 @@ import sys
 from musket_server import server, tasks, setup_server
 
 def sysarg(num):
-    sys.argv[num] if len(sys.argv > num) else None
+    sys.argv[num] if len(sys.argv) > num else None
 
 def main():
     if sysarg(1) == "setup":
         setup_server.run(sysarg(2) , sysarg(3), sysarg(4))
 
         return
-    
+
     server.start_server(int(sysarg(1)), tasks.TaskManager(2))
 
 #main()
